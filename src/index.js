6650d1b9ee9afa29c5316646292cc1227e975f23
Hunter Lombard <hwlombard@gmail.com>
2017-03-15 17:54:27 -0400

    Set up skeleton and style css

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import { Route, Router, browserHistory } from 'react-router';
 
+import "./styles/style.css"
 import "./vendor/css/skeleton_css/normalize.css"
 import "./vendor/css/skeleton_css/skeleton_css.css"
 
